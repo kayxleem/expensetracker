8292f5a858f72033a73b6e88a10bfe573cde342e
Adegbuyi Oluwakayode <kayode_adegbuyi@yahoo.com>
2023-03-17 15:24:10 +0100

    updated migration

--- database/migrations/2023_03_12_010459_create_expenses_table.php
+++ database/migrations/2023_03_12_010459_create_expenses_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('expenses', function (Blueprint $table) {
             $table->id();
+            $table->string('date');
+            $table->string('merchant');
+            $table->integer('total');
+            $table->string('status')->default('Reimbursed');
+            $table->string('description', 255)->nullable();
+            $table->string('image')->nullable();
+            $table->longText('comment');
             $table->timestamps();
         });
     }
