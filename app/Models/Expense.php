fa9226f9a20cc0d8c2b266e08075018f21e111e0
Adegbuyi Oluwakayode <kayode_adegbuyi@yahoo.com>
2023-03-17 15:25:06 +0100

    update model

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -2,10 +2,35 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Expense extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+
+    public function scopeFrom(Builder $query, $date): Builder
+    {
+        return $query->where('date', '<=', Carbon::parse($date));
+    }
+
+    public function scopeTo(Builder $query, $date): Builder
+    {
+        return $query->where('date', '>=', Carbon::parse($date));
+    }
+
+    public function scopeMin(Builder $query, $total): Builder
+    {
+        return $query->where('total', '>=', $total);
+    }
+
+    public function scopeMax(Builder $query, $total): Builder
+    {
+        return $query->where('total', '<=', $total);
+    }
 }
